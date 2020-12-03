eab5971f15ac8cadf3e1aeeebd68d56f865710b5
Naldo Wang <1165648703@qq.com>
2020-12-03 17:23:10 +0800

    添加接地图形的锚点

--- src/diagram/circuit-diagram/ground/ground.anchor.js
+++ src/diagram/circuit-diagram/ground/ground.anchor.js
@@ -1,4 +1,8 @@
-// import { Node } from '@topology/core'
+import { Point, Direction } from '@topology/core'
 
 export function circuitGroundAnchors (node) {
+  node.anchors.push(new Point(node.rect.x + node.rect.width / 2, node.rect.y, Direction.Up))
+  node.anchors.push(new Point(node.rect.x, node.rect.y + node.rect.height / 2, Direction.Left))
+  node.anchors.push(new Point(node.rect.x + node.rect.width / 2, node.rect.ey, Direction.Bottom))
+  node.anchors.push(new Point(node.rect.ex, node.rect.y + node.rect.height / 2, Direction.Right))
 }
